51319505dab7ba091e628b7649d61652befd4c74
Cliffton Hicks <cliffton.hicks@gmail.com>
2016-12-16 17:50:53 -0500

    version 0.6.1.dev1
    Added setup.py and Mainifest for readme -currently on testing purpose and pip installation

---
+++ setup.py
@@ -0,0 +1,25 @@
+import io
+import os
+from setuptools import setup
+
+#Encoding specification for PY3
+with io.open('README.md', encoding='utf-8') as fp:
+    description = fp.read()
+
+#check and tweak version number
+#can change author and email if necessary
+setup(
+    name = 'ccRender',
+    version = '0.6.1.dev1',
+    author = 'Cliffton Hicks',
+    author_email = 'cliffton@omnibond.com',
+    license = 'LGPL',
+    url = 'https://github.com/omnibond/ccRender',
+    description = 'Cloud-based Blender rendering addon',
+    long_description = description,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Progarming Language :: Python :: 3.5',
+    ],
+    install_requires = ['scp'],
+)
